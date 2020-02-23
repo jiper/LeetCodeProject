2ca25e2faaa5d7ace8dcd46b98a56157076e4dc5
jiper <542062320@qq.com>
2020-02-23 22:21:40 +0800

    2020-2-23

--- test.cpp
+++ test.cpp
@@ -1,16 +1,26 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int romanToInt(string s) {
+    int romanToInt(string s)
+    {
         return 1;
     }
 };
 
 int main()
 {
-    cout <<"fuck you!"<<endl;
-    return 0;
+  vector<string> inputV;
+  vector<string>::iterator iter;
+  string inputS;
+  while(cin >>inputS)
+  {
+      inputV.insert(inputV.end(),inputS);
+  }
+  for(iter=inputV.begin();iter<inputV.end();iter++)
+    cout << *iter << " ";
+  return 0;
 }
-
